b6ca44e15a41d8b648bfe04debf43c00d99c767b
leoCardosoDev <leocardosodev@gmail.com>
2023-10-24 19:04:47 -0300

    feat: add busca linear

--- Capitulo_02/01_algoritmos_de_ordenacao/02_linear_search/01_0_linear_search.py
+++ Capitulo_02/01_algoritmos_de_ordenacao/02_linear_search/01_0_linear_search.py
@@ -1,11 +1,12 @@
-def linear_search (arr, v):
-  for i in range(len(arr)):
-    if arr[i] == v:
-      return i
-  return 'NIL'
+def busca_linear(arr, v):
+    for i in range(len(arr)):
+        if arr[i] == v:
+            return i
+    return 'NIL'
 
-a = [10, 20, 30, 40, 50]
-v = 30
+# Sequência e valor a ser procurado
+sequencia = [10, 20, 30, 40, 50]
+valor_procurado = 30
 
 # Invariante de Loop:
 # A cada iteração, o algoritmo verifica se o elemento atual é igual a v. Se encontrar, retorna o índice i.
@@ -18,15 +19,16 @@ v = 30
 
 # Executando a busca linear
 
-result = linear_search(a,v)
-if result != 'NIL':
-  print(f'O valor {v} foi encontrado no indice {result}')
+resultado = busca_linear(sequencia, valor_procurado)
+if resultado != 'NIL':
+    print(f'O valor {valor_procurado} foi encontrado no índice {resultado}')
 else:
-  print(f'O valor {v} não foi encontrado na sequência')
+    print(f'O valor {valor_procurado} não foi encontrado na sequência')
 
-v = 25
-result = linear_search(a, v)
-if result != 'NIL':
-  print(f'O valor {v} foi encontrado no indice {result}')
+# Executando a busca para um valor que não está na sequência
+valor_nao_encontrado = 25
+resultado = busca_linear(sequencia, valor_nao_encontrado)
+if resultado != 'NIL':
+    print(f'O valor {valor_nao_encontrado} foi encontrado no índice {resultado}')
 else:
-  print(f'O valor {v} não foi encontrado na sequência')
+    print(f'O valor {valor_nao_encontrado} não foi encontrado na sequência')
