5fdad918f05ee57d8293236b6ac9451662f8ee36
leoCardosoDev <leocardosodev@gmail.com>
2023-10-24 19:01:50 -0300

    feat: add 03_ notação big O com graficos

--- Capitulo_02/01_algoritmos_de_ordenacao/02_linear_search/03_notacao_big_o.py
+++ Capitulo_02/01_algoritmos_de_ordenacao/02_linear_search/03_notacao_big_o.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Função f(n)
-def f(n):
+# Definindo a função f(n)
+def funcao_f(n):
     return (n**3 / 1000) - 10 * n**2 - 100 * n + 3
 
-# Valores de n
-n = np.arange(1, 101)  # Vamos considerar n de 1 a 100
+# Gerando uma série de valores de n de 1 a 100
+valores_de_n = np.arange(1, 101)
 
-# Calcula f(n) para cada valor de n
-result = [f(i) for i in n]
+# Calculando f(n) para cada valor de n
+resultados_f = [funcao_f(valor) for valor in valores_de_n]
 
-# Plot da função f(n)
+# Criando um gráfico da função f(n)
 plt.figure(figsize=(10, 6))
-plt.plot(n, result, label='f(n) = (n^3 / 1000) - 10n^2 - 100n + 3', color='red')
+plt.plot(valores_de_n, resultados_f, label='f(n) = (n^3 / 1000) - 10n^2 - 100n + 3', color='red')
 plt.xlabel('n')
 plt.ylabel('f(n)')
 plt.title('Comportamento de f(n) em relação a n')
